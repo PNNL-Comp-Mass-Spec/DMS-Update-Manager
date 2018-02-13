c4ee774daaed462955228a9a5a5a5285c7192686
alchemistmatt <matthew.monroe@pnnl.gov>
2018-02-12 21:18:41 -0800

    Remove unused class and update the program date

--- DMSUpdateManagerConsole/Program.cs
+++ DMSUpdateManagerConsole/Program.cs
@@ -22,7 +22,7 @@ namespace DMSUpdateManagerConsole
         /// <summary>
         /// Program date
         /// </summary>
-        public const string PROGRAM_DATE = "February 9, 2018";
+        public const string PROGRAM_DATE = "February 12, 2018";
 
         // Either mSourceFolderPath and mTargetFolderPath must be specified, or mParameterFilePath needs to be specified
 
