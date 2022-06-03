3361459cba7c503ac958a61f356168ec95a3d254
alchemistmatt <matthew.monroe@pnnl.gov>
2022-06-03 10:28:37 -0700

    Update SSH.NET from NuGet

--- DMSUpdateManager.cs
+++ DMSUpdateManager.cs
@@ -35,7 +35,7 @@ namespace DMSUpdateManager
         /// </summary>
         public DMSUpdateManager()
         {
-            mFileDate = "April 27, 2022";
+            mFileDate = "June 3, 2022";
 
             mFilesToIgnore = new SortedSet<string>(StringComparer.OrdinalIgnoreCase);
             mProcessesDict = new Dictionary<uint, ProcessInfo>();
